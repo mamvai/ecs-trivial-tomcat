9651778ad63a5b778ec11ce3a1b6f5f97cd93d4f
mamvai <70385979+mamvai@users.noreply.github.com>
2020-11-03 16:08:41 -0500

    added cli commands

--- src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
+++ src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
@@ -4,7 +4,9 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
@@ -26,9 +28,52 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 		out.print("Hello from Crunchify servlet !!!");
 		System.out.println("Test.. Test by CrunchifyMaven.. \nThis is a simple tutorial  using Maven Plugin..");
 		
+		String yourCliCommandWithArgs = "curl http://169.254.169.254/latest/meta-data/local-ipv4";
+		ProcessBuilder pb = new ProcessBuilder( yourCliCommandWithArgs );
+		Process process = null;
+		try {
+			process = pb.start();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line;
+		
+		try {
+			while ((line = br.readLine()) != null) {
+			    out.println(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
 	public static void main(String[] args) {
 		System.out
 				.println("In Main Test.. Test by CrunchifyMaven.. This is a simple tutorial  using Maven Plugin..");
+		
+		String yourCliCommandWithArgs = "curl http://169.254.169.254/latest/meta-data/local-ipv4";
+				ProcessBuilder pb = new ProcessBuilder( yourCliCommandWithArgs );
+				Process process = null;
+				try {
+					process = pb.start();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+		String line;
+
+        try {
+			while ((line = br.readLine()) != null) {
+			    System.out.println(line);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }

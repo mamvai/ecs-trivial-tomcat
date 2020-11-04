2a66adf3f21bbee64061b91d2e02a42a52a0fb48
mamvai <70385979+mamvai@users.noreply.github.com>
2020-11-04 14:17:52 -0500

    running state

--- src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
+++ src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
@@ -83,7 +83,7 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 				+ "export PATH=$PATH:/usr/local/bin ; echo $PATH ;  chmod +x /usr/local/bin/ecs-cli ; /usr/local/bin/ecs-cli --version ; ecs-cli --version ; "
 				+ "ecs-cli configure -region us-east-2 -cluster ecs-trivial-cluster-ECSCluster-2COG3zXz2iAj ;"
 				+ "ecs-cli configure profile --access-key ASIARIWBPPCD4WAZPFO7 --secret-key eeaoKHcXk1I41Opf+jIO3y1BdSNv9/1dGGu4kK8X --session-token IQoJb3JpZ2luX2VjEIH//////////wEaCXVzLWVhc3QtMSJIMEYCIQCIiB/c4oT9pzjJiAW6fH3yM8MrpAMX0Tjt2RLYKzb/VwIhAIpw92M9gt5jzhEb6h6ZpWWPrRJIQfEmpWS1eJ2GkynEKqYCCNr//////////wEQABoMMDg3Mzc4ODUxOTc1IgwekXiB0QnmI0mAFdcq+gG27ns2k1Oc24NevXsM2RSYZwSVWilVbonK63GlTBT16p0/XT65yAE1YTCkQpfsp4V56RV56ej8vu2UfY4gnNxGZZBIpx8aVPP+4NbiSaj2P+Lr+KL3ogp0EofTA9lnD2v+2//ONJ5khec3TKu4YRzZ7HqYr4gzJmGVGBV5tWvmLXSzazkcBpAFrisAEl5Jdlr5W43YMobc3xquEIrJHOAWKI4ZVtlEykxCxNF6r9RVTMFhC9KI8OcmS1Vj23+iB5hEnMau7taFbIsoWTQclCO1D1dTXm1M7r/dZ6FeM9h9UNh8T/Gji9aBxTJ8re21MWySq7Az6Z8c17fGMJ6ri/0FOpwBBjkqhS5U9mKwzZfN+Ll+zlsf4YG5ejVqwYV4rjaMnfqsUcE8swLFMThczfs4ba6wfa2PawHBJjvKlI6unJAm6rKpH1dlXQDx0ksZNvERKDasNfCW4ROLIEYr6wRCj7PKzYMYuo0X9XfAwbQRE02xOw3cdCXCuq6k406rDI0uX0RO1gX5Z1QUGQ+PNGOGzWhAlrP/XEBt+jjFalTT; "
-				+ "ecs-cli ps ; /usr/local/bin/ecs-cli ps");
+				+ "ecs-cli ps ; /usr/local/bin/ecs-cli ps --desired-status RUNNING");
 		runCLICmd(yourCliCommandWithArgs,out,pb);
 		
 		//pb = new ProcessBuilder ("sh", "-c", "curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip  ; yum install unzip; unzip awscliv2.zip ;  ./aws/install ; export PATH=~/bin:$PATH ; aws --version ");

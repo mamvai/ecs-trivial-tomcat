8a8c15ee841511e13b48263d419a97a54b7f4e5f
mamvai <70385979+mamvai@users.noreply.github.com>
2020-11-06 11:47:09 -0500

    added redis

--- src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
+++ src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
@@ -4,6 +4,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.redisson.Redisson;
+import org.redisson.api.RBucket;
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+
+import crunchify.com.tutorials.pojo.SessionInfo;
+import redis.clients.jedis.Jedis;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -31,10 +40,49 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 	                                   throws IOException,ServletException{
 		ServletOutputStream out=response.getOutputStream();
 		out.print("Hello from Crunchify servlet !!!");
-		System.out.println("Test.. Test by CrunchifyMaven.. \nThis is a simple tutorial  using Maven Plugin..");
+		System.out.println("Test.. Test by Redis CrunchifyMaven.. \nThis is a simple tutorial  using Maven Plugin..");
 		
-		//runCLICmd("aws ecs list-container-instances",out);
-		String yourCliCommandWithArgs = "export NO_PROXY=169.254.169.254,169.254.170.2 ; curl http://169.254.169.254/latest/meta-data/local-ipv4";
+		// Connecting to Redis server on localhost 
+	    // Jedis jedis = new Jedis("192.168.86.47",7001); 
+		Jedis jedis = new Jedis("epacache-ro.50fbfo.ng.0001.use2.cache.amazonaws.com",6379);
+	    
+	    System.out.println("Connection to redis server sucessfully"); 
+	    //check whether server is running or not 
+	    System.out.println("Redis Server is running: "+jedis.ping()); 
+	    
+	    /*
+
+	    SessionInfo info = new SessionInfo();
+	    // Sync and Async API
+	    try{
+	        Config conf = new Config();
+	        conf.useSingleServer().setTimeout(3600000);
+	        conf.useSingleServer().setRetryInterval(3600000);
+	        conf.useSingleServer().setAddress("redis://192.168.86.47:7001");
+	        
+	        RedissonClient redisson = Redisson.create(conf);
+	        RMap<String,Object> map = redisson.getMap("myCache");
+	        map.put("key", info);
+	        
+	       
+	        redisson.shutdown();
+	        
+	        
+	        redisson = Redisson.create(conf);
+	        RMap<String,Object> getmap = redisson.getMap("myCache");
+	        SessionInfo obj = (SessionInfo) getmap.get("key");
+	        out.println("\nredis object = " + obj.getSessionID());
+	        out.println("redis user object = " + obj.getUserObject().getName());
+	        out.println("redis user object = " + obj.getUserObject().getFullName());
+	        out.println("redis calendar object = " + obj.getLastAccess().getTime().getTime());
+	        
+	    }catch (Exception ex) {
+	        ex.printStackTrace();
+	    }
+	    */
+		
+		 //runCLICmd("aws ecs list-container-instances",out);
+		 String yourCliCommandWithArgs = "export NO_PROXY=169.254.169.254,169.254.170.2 ; curl http://169.254.169.254/latest/meta-data/local-ipv4";
 		
 		ProcessBuilder pb = new ProcessBuilder ("sh", "-c", "export NO_PROXY=169.254.169.254,169.254.170.2 ; curl http://169.254.169.254/latest/meta-data/local-ipv4");
 		//working -  runCLICmd(yourCliCommandWithArgs,out,pb);
@@ -80,13 +128,15 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 		//ecs-cli configure -region us-east-2 -cluster ecs-trivial-cluster-ECSCluster-2COG3zXz2iAj
 		//ecs-cli configure profile --access-key ASIARIWBPPCD4WAZPFO7 --secret-key eeaoKHcXk1I41Opf+jIO3y1BdSNv9/1dGGu4kK8X
 		//ecs-cli ps --aws-profile default --cluster ecs-trivial-cluster-ECSCluster-2COG3zXz2iAj --desired-status RUNNING | grep Ports
-		pb = new ProcessBuilder ("sh", "-c", "curl -Lo /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest ; find . -name ecs-cli -print ; "
+		
+		/* working
+		 * pb = new ProcessBuilder ("sh", "-c", "curl -Lo /usr/local/bin/ecs-cli https://amazon-ecs-cli.s3.amazonaws.com/ecs-cli-linux-amd64-latest ; find . -name ecs-cli -print ; "
 				+ "export PATH=$PATH:/usr/local/bin ; echo $PATH ;  chmod +x /usr/local/bin/ecs-cli ; /usr/local/bin/ecs-cli --version ; ecs-cli --version ; "
 				+ "ecs-cli configure -region us-east-2 -cluster ecs-trivial-cluster-ECSCluster-2COG3zXz2iAj ;"
 				+ "ecs-cli configure profile --access-key ASIARIWBPPCD4WAZPFO7 --secret-key eeaoKHcXk1I41Opf+jIO3y1BdSNv9/1dGGu4kK8X --session-token IQoJb3JpZ2luX2VjEIH//////////wEaCXVzLWVhc3QtMSJIMEYCIQCIiB/c4oT9pzjJiAW6fH3yM8MrpAMX0Tjt2RLYKzb/VwIhAIpw92M9gt5jzhEb6h6ZpWWPrRJIQfEmpWS1eJ2GkynEKqYCCNr//////////wEQABoMMDg3Mzc4ODUxOTc1IgwekXiB0QnmI0mAFdcq+gG27ns2k1Oc24NevXsM2RSYZwSVWilVbonK63GlTBT16p0/XT65yAE1YTCkQpfsp4V56RV56ej8vu2UfY4gnNxGZZBIpx8aVPP+4NbiSaj2P+Lr+KL3ogp0EofTA9lnD2v+2//ONJ5khec3TKu4YRzZ7HqYr4gzJmGVGBV5tWvmLXSzazkcBpAFrisAEl5Jdlr5W43YMobc3xquEIrJHOAWKI4ZVtlEykxCxNF6r9RVTMFhC9KI8OcmS1Vj23+iB5hEnMau7taFbIsoWTQclCO1D1dTXm1M7r/dZ6FeM9h9UNh8T/Gji9aBxTJ8re21MWySq7Az6Z8c17fGMJ6ri/0FOpwBBjkqhS5U9mKwzZfN+Ll+zlsf4YG5ejVqwYV4rjaMnfqsUcE8swLFMThczfs4ba6wfa2PawHBJjvKlI6unJAm6rKpH1dlXQDx0ksZNvERKDasNfCW4ROLIEYr6wRCj7PKzYMYuo0X9XfAwbQRE02xOw3cdCXCuq6k406rDI0uX0RO1gX5Z1QUGQ+PNGOGzWhAlrP/XEBt+jjFalTT; "
 				+ "ecs-cli ps ; /usr/local/bin/ecs-cli ps --desired-status RUNNING");
 		runCLICmd(yourCliCommandWithArgs,out,pb);
-		
+		*/
 		//pb = new ProcessBuilder ("sh", "-c", "curl https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip  ; yum install unzip; unzip awscliv2.zip ;  ./aws/install ; export PATH=~/bin:$PATH ; aws --version ");
 		//runCLICmd(yourCliCommandWithArgs,out,pb);
 		
@@ -147,9 +197,10 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 			    	if ( st.nextToken().equals("RUNNING")) {
 			    		publicIP = st.nextToken();
 			    		StringTokenizer stip = new StringTokenizer(publicIP,":");
+			    		out.println("publicIP with port =" + publicIP);
 			    		while(stip.hasMoreTokens()) {
 			    			String IP =stip.nextToken();
-			    			System.out.println("PUBLIC IP = " + IP);
+			    			out.println("PUBLIC IP = " + IP);
 			    			break ;
 			    		}
 			    		break;
@@ -164,7 +215,7 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 			e.printStackTrace();
 		}
 	}
-	
+	/*
 	public static void main(String[] args) {
 		System.out
 				.println("In Main Test.. Test by CrunchifyMaven.. This is a simple tutorial  using Maven Plugin..");
@@ -189,5 +240,5 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	}
+	}*/
 }

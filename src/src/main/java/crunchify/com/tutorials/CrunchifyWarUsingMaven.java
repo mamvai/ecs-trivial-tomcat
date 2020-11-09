6759faec03828d2dd045bfaf16ad939ea5f57098
mamvai <70385979+mamvai@users.noreply.github.com>
2020-11-09 11:33:10 -0500

    cache node

--- src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
+++ src/src/main/java/crunchify/com/tutorials/CrunchifyWarUsingMaven.java
@@ -47,7 +47,7 @@ public class CrunchifyWarUsingMaven extends HttpServlet{
 		// Connecting to Redis server on localhost 
 	    // Jedis jedis = new Jedis("192.168.86.47",7001); 
 		
-		Jedis jedis = new Jedis("epacache-cluster-0001-001.50fbfo.0001.use2.cache.amazonaws.com",6379);
+		Jedis jedis = new Jedis("epacache-001.50fbfo.0001.use2.cache.amazonaws.com",6379);
 								 
 	    
 	    out.println("Connection to redis server sucessfully"); 
